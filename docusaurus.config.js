19bea52b58278649c3723fb3bff0d61f29a218d0
tristangnl <tristan.gnl@gmail.com>
2026-05-03 16:46:26 +0100

    add edit github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/tristangnl/3ds-circle-pad-pro-wiki/tree/main',
         },
         blog: false,
         theme: {
